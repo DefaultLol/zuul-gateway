eb7ba72523ca7e068493a0812c6b66cd9a4f6502
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-01-29 22:22:33 +0100

    add cors option 5

--- src/main/java/com/app/demo/config/GatewayConfiguration.java
+++ src/main/java/com/app/demo/config/GatewayConfiguration.java
@@ -6,10 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -17,10 +13,10 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable().cors().configurationSource(corsConfigurationSource()).and()
+            .csrf().disable()
             .authorizeRequests()
             .antMatchers("/oauth/**").permitAll()
-            .antMatchers("/api/client/**").hasRole("AGENT")
+            .antMatchers("/api/client/**").permitAll()
             .antMatchers("/api/account/**").permitAll()
                 .antMatchers("/cmi/**").hasRole("CLIENT");
     }
@@ -32,20 +28,5 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
         tokenService.setClientId("client");
         tokenService.setClientSecret("secret");
         return tokenService;
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        configuration.setAllowCredentials(true);
-        //the below three lines will add the relevant CORS response headers
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
